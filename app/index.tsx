082250e2c5b99ada6bd429d2e2dd287b514995e4
Neel <neelx2107@gmail.com>
2025-01-31 21:12:58 +0530

    feat: dependecies install & dev build

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RootScreen = () => {
+  return (
+    <View className='flex-1 bg-blue-200' >
+      <Text>RootScreen</Text>
+    </View>
+  )
+}
+
+export default RootScreen
